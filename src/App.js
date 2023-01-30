8a0270e6897bca7e95526955c03aa981c2b2829d
Ece Çakmak <86318154+ececmk@users.noreply.github.com>
2023-01-30 20:19:44 +0100

    project done

--- src/App.js
+++ src/App.js
@@ -4,7 +4,50 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 
 function App() {
-  return <h2>color generator project</h2>
+  const [color, setColor] = useState('');
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      let colors = new Values(color).all(10)
+      setList(colors)
+    } catch (error) {
+      setError(true)
+      console.log(error)
+    }
+  }
+
+
+
+  return (
+    <>
+      <section className='container'>
+        <h3>Color Generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input type='text' 
+            value={color} 
+            onChange={(e) => setColor(e.target.value)}  
+            placeholder='#f15025'
+            className={`${error ? 'error' : null}`}/>
+          <button className='btn' type='submit'>Submit  </button>
+        </form>
+      </section>
+      <section className='colors'>
+      {list.map((color, index) => {
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          )
+        })}
+      </section>
+    </>
+  )
 }
 
 export default App
